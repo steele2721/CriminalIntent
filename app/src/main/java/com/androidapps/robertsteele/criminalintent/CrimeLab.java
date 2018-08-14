dffae6bc1629eb3798d1d8a6457852a5ffa4223d
robertsteele <steele.210@osu.edu>
2018-08-13 21:18:50 -0400

    Created CrimeBaseHelper Class and CrimeDBSchema adding and updating logic added for Crime database added onPause to CrimeFragment for pushing data

--- app/src/main/java/com/androidapps/robertsteele/criminalintent/CrimeLab.java
+++ app/src/main/java/com/androidapps/robertsteele/criminalintent/CrimeLab.java
@@ -1,6 +1,13 @@
 package com.androidapps.robertsteele.criminalintent;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.androidapps.robertsteele.criminalintent.database.CrimeBaseHelper;
+import com.androidapps.robertsteele.criminalintent.database.CrimeDBSchema.CrimeTable;
+import com.androidapps.robertsteele.criminalintent.database.CrimeDBSchema.CrimeTable.Cols;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -8,8 +15,8 @@ import java.util.UUID;
 public class CrimeLab {
 
     private static CrimeLab sCrimeLab;
-
-    private List<Crime> mCrimes;
+    private Context mcontext;
+    private SQLiteDatabase mSQLiteDatabase;
 
     public static CrimeLab get(Context context) {
         if (sCrimeLab == null) {
@@ -18,24 +25,37 @@ public class CrimeLab {
         return sCrimeLab;
     }
 
+    public static ContentValues getContentValues(Crime crime) {
+        ContentValues contentValues = new ContentValues();
+        contentValues.put(Cols.UUID, crime.getmId().toString());
+        contentValues.put(Cols.DATE, crime.getmDate().toString());
+        contentValues.put(Cols.TITLE, crime.getMtitle());
+        contentValues.put(Cols.SOLVED, crime.ismSolved() ? 1 : 0);
+        return contentValues;
+    }
+
     public void addCrime(Crime crime) {
-        mCrimes.add(crime);
+        ContentValues contentValues = getContentValues(crime);
+        mSQLiteDatabase.insert(CrimeTable.NAME, null, contentValues);
+    }
+
+    public void updateCrime(Crime crime) {
+        String uuidString = crime.getmId().toString();
+        ContentValues contentValues = getContentValues(crime);
+        mSQLiteDatabase.update(CrimeTable.NAME, contentValues,
+                Cols.UUID + " = ? ", new String[] { uuidString });
     }
 
     private CrimeLab(Context context) {
-        mCrimes = new ArrayList<>();
+        mcontext = context.getApplicationContext();
+        mSQLiteDatabase = new CrimeBaseHelper(mcontext).getReadableDatabase();
     }
 
     public List<Crime> getmCrimes() {
-        return mCrimes;
+        return new ArrayList<Crime>();
     }
 
     public Crime getCrime(UUID uuid) {
-        for (Crime crime : mCrimes) {
-            if (crime.getmId().equals(uuid)) {
-                return crime;
-            }
-        }
         return null;
     }
 }
